cf8bfaf5bd6a596a21cf89a59184a5fde15dcddf
mrvollger <mrvollger@gmail.com>
2020-04-05 08:42:01 -0700

    Simplified zmw check function and udpated tests

--- data/cmds.sh
+++ data/cmds.sh
@@ -2,10 +2,15 @@
 
 set -euo pipefail
 
-../minimap2 --zmw-hit-only --eqx -Y -ax map-pb fake.ccs.fasta 25.fastq | samtools view -b -F 4 | samtools sort > tmp.bam 
+../minimap2 --zmw-hit-only --eqx -Y -ax map-pb fake.ccs.fasta median.fastq | samtools view -b -F 4 | samtools sort | pbbamify fake.ccs.fasta median.bam  >  pb.pipe.bam 
 
-pbbamify fake.ccs.fasta median.bam --input tmp.bam --output pb.tmp.bam   
+../minimap2 --eqx -Y -ax map-pb fake.ccs.fasta median.fastq | samtools view -b -F 4 | samtools sort | pbbamify fake.ccs.fasta median.bam  >  pb.2.pipe.bam 
+
+for f in pb.pipe.bam pb.2.pipe.bam; do 
+	echo $f
+	samtools view $f | awk '{print $1"\t"$3}' | sort | uniq  > $f.summary
+done 
+
+diff pb.pipe.bam.summary pb.2.pipe.bam.summary  > diff.sum
 
 
-
-../minimap2 --zmw-hit-only --eqx -Y -ax map-pb fake.ccs.fasta 25.fastq | samtools view -b -F 4 | samtools sort | pbbamify fake.ccs.fasta median.bam  >  pb.pipe.bam 
--- map.c
+++ map.c
@@ -123,34 +123,21 @@ static mm_match_t *collect_matches(void *km, int *_n_m, int max_occ, const mm_id
 }
 
 // MRV addition, if zmw and movies are the same returns 1, else 0
-static inline int share_zmw(const char *qname_const, const char *tname_const)
+static inline int share_zmw(const char *qname, const char *tname)
 {
-	//fprintf(stderr, "\e[01;32mTop of check zmw %s %s \e[0m\n", qname_const, tname_const);
-	// make string copies 
-	char* qname = calloc(strlen(qname_const)+1, sizeof(char)); strcpy(qname, qname_const);
-	char* tname = calloc(strlen(tname_const)+1, sizeof(char)); strcpy(tname, tname_const);
-
-	// Extract the query movie / zmq
-   	char * q_movie = strtok_r(qname, "/", &qname);
-   	char * q_zmw   = strtok_r(qname, "/", &qname);
-	if(q_movie == NULL || q_zmw == NULL ){
-		fprintf(stderr, "\e[01;31mQuery seq not in PacBio format, skipping: %s\n\e[0m", qname_const);
-		return(0);
+	int counter = 0; int idx = 0;
+	char qchar = qname[idx]; char tchar = tname[idx];
+	while(qchar == tchar && qchar != '\0' && tchar != '\0'){
+		if( qchar == '/'){
+			counter++; 
+			if(counter == 2){	
+				return(1);
+			}
+		}	
+		idx++;
+		qchar = qname[idx]; tchar = tname[idx];
 	}
-
-   	// Extract the target movie / zmq
-   	char * t_movie = strtok_r(tname, "/", &tname);
-   	char * t_zmw   = strtok_r(tname, "/", &tname);
-	if(t_movie == NULL || t_zmw == NULL ){
-		fprintf(stderr, "\e[01;31mTarget seq not in PacBio format, skipping: %s\n\e[0m", tname_const);
-		return(0);
-	}
-	
-	//fprintf(stderr, "\e[01;31m %s %s: %s %s\n\e[0m", t_movie, t_zmw, q_movie, q_zmw);
-	if( strcmp(t_movie, q_movie) == 0 && strcmp(t_zmw, q_zmw) == 0 ){
-		return(1);
-	}
-	//fprintf(stderr, "\e[01;31mZMWs do not match, skipping: %s, %s\n\e[0m", tname_const, qname_const);
+	//fprintf(stderr, "\e[01;31m Not matching or not in PacBio header format: %s %s \e[0m\n", qname, tname);
 	return(0);
 }
 // MRV end
