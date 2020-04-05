e3781e37ed5531d2d016f2ccf99584e83ce88c86
mrvollger <mrvollger@gmail.com>
2020-04-05 08:44:30 -0700

    changed names

--- data/cmds.sh
+++ data/cmds.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 
 ../minimap2 --zmw-hit-only --eqx -Y -ax map-pb fake.ccs.fasta median.fastq | samtools view -b -F 4 | samtools sort | pbbamify fake.ccs.fasta median.bam  >  pb.pipe.bam 
 
-../minimap2 --eqx -Y -ax map-pb fake.ccs.fasta median.fastq | samtools view -b -F 4 | samtools sort | pbbamify fake.ccs.fasta median.bam  >  pb.2.pipe.bam 
+../minimap2 --eqx -Y -ax map-pb fake.ccs.fasta median.fastq | samtools view -b -F 4 | samtools sort | pbbamify fake.ccs.fasta median.bam  >  pb.keep.all.pipe.bam 
 
-for f in pb.pipe.bam pb.2.pipe.bam; do 
+for f in pb.pipe.bam pb.keep.all.pipe.bam; do 
 	echo $f
 	samtools view $f | awk '{print $1"\t"$3}' | sort | uniq  > $f.summary
+	samtools view -c $f
 done 
 
-diff pb.pipe.bam.summary pb.2.pipe.bam.summary  > diff.sum
+diff pb.pipe.bam.summary pb.keep.all.pipe.bam.summary  > diff.sum
 
 
