9a9b3a7a01467895ff6f8be31754d01383168530
Mitchell Vollger <mrvollger@Mitchells-MacBook-Pro.local>
2020-04-04 21:24:19 -0700

    added minimal testcases and data

---
+++ data/cmds.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+../minimap2 --zmw-hit-only --eqx -Y -ax map-pb fake.ccs.fasta 25.fastq | samtools view -b -F 4 | samtools sort > tmp.bam 
+
+pbbamify fake.ccs.fasta median.bam --input tmp.bam --output pb.tmp.bam   
+
