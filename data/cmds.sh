86d04d75180653d919954213fd23ebac2b926d49
Mitchell Vollger <mrvollger@Mitchells-MacBook-Pro.local>
2020-04-04 22:16:10 -0700

    add pipe example with pbbamify

--- data/cmds.sh
+++ data/cmds.sh
@@ -6,3 +6,6 @@ set -euo pipefail
 
 pbbamify fake.ccs.fasta median.bam --input tmp.bam --output pb.tmp.bam   
 
+
+
+../minimap2 --zmw-hit-only --eqx -Y -ax map-pb fake.ccs.fasta 25.fastq | samtools view -b -F 4 | samtools sort | pbbamify fake.ccs.fasta median.bam  >  pb.pipe.bam 
